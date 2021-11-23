882dd42fea5242d272c3a9ef0a49f6c16b1c108d
Joshua Insorio <55485720+joshinsorio@users.noreply.github.com>
2021-11-22 20:01:02 -0800

    Problem 4 stuck. Halfway through problem 2
    Not sure how to do pointwise multiplication

--- main.cpp
+++ main.cpp
@@ -28,60 +28,202 @@ void fft2d(int N, int M, double **real_fuv, double **image_fuv, int isign);
 void imgtodata(ImageType &image, float **data);
 void datatoimg(ImageType &image, float **data);
 
+void experiment2(char fname[]);
 void experiment4(char fname[]);
+
+const float sobel_mask[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
 
 int main(int argc, char *argv[])
 {
 	char girl[] = "girl.pgm";
+	char boy[] = "boy_noisy.pgm";
+	char lenna[] = "lenna.pgm";
 
-	experiment4(girl);
+	experiment2(lenna);
+	// experiment4(girl);
 
 	return 0;
+}
+
+void experiment2(char fname[])
+{
+	ImageType baseImage(512, 512, 255);
+	readImage(fname, baseImage);
+
+	// Spatial Filter
+	int temp;
+	float pixels[256 * 256];
+	for (int i = 0; i < 256; i++)
+	{
+		for (int j = 0; j < 256; j++)
+		{
+			float result = 0;
+
+			for (int k = -1; k < 2; k++)
+			{
+				for (int l = -1; l < 2; l++)
+				{
+					if (i + k < 0 || i + k >= 256 || j + l < 0 || j + l >= 256)
+					{
+						result += 0;
+					}
+
+					else
+					{
+						baseImage.getPixelVal(i + k, j + l, temp);
+						result += temp * sobel_mask[3 / 2 - k][3 / 2 - l];
+					}
+				}
+			}
+			pixels[i * 256 + j] = result;
+		}
+	}
+
+	// Normalization
+	float max = INT_MIN;
+	float min = INT_MAX;
+
+	for (int i = 0; i < 256; i++)
+	{
+		for (int j = 0; j < 256; j++)
+		{
+			if (pixels[i * 256 + j] > max)
+			{
+				max = pixels[i * 256 + j];
+			}
+			if (pixels[i * 256 + j] < min)
+			{
+				min = pixels[i * 256 + j];
+			}
+		}
+	}
+
+	ImageType finalImage_spatial(256, 256, 255);
+	char finImage_spatial[] = "part2_spatial.pgm";
+	for (int i = 0; i < 256; i++)
+	{
+		for (int j = 0; j < 256; j++)
+		{
+			temp = (int)(255.0 * (((double)pixels[i * 256 + j] - min) / (double)(max - min)));
+			finalImage_spatial.setPixelVal(i, j, temp);
+		}
+	}
+	writeImage(finImage_spatial, finalImage_spatial);
+
+	// Frequency Filter
+	// double **real_fuv = new double *[256];
+	// for (int i = 0; i < 256; i++)
+	// {
+	// 	real_fuv[i] = new double[256 * 2 + 1];
+	// }
+	// double **image_fuv = new double *[256];
+	// for (int i = 0; i < 256; i++)
+	// {
+	// 	image_fuv[i] = new double[256 * 2 + 1];
+	// }
+
+	// for (int i = 0; i < 256; i++)
+	// {
+	// 	for (int j = 0; j < 256; j++)
+	// 	{
+	// 		baseImage.getPixelVal(i, j, temp);
+	// 		real_fuv[i][j] = temp;
+	// 		image_fuv[i][j] = 0;
+	// 	}
+	// }
+
+	// fft2d(256, 256, real_fuv, image_fuv, 1);
+
+	// double **real_fuv2 = new double *[256];
+	// for (int i = 0; i < 256; i++)
+	// {
+	// 	real_fuv2[i] = new double[256 * 2 + 1];
+	// }
+	// double **image_fuv2 = new double *[256];
+	// for (int i = 0; i < 256; i++)
+	// {
+	// 	image_fuv2[i] = new double[256 * 2 + 1];
+	// }
+
+	// 	//Initialize mask inside
+	// for (int i = 0; i < 3; i++)
+	// {
+	// 	for (int j = 0; j < 3; j++)
+	// 	{
+	// 		int x = 256 / 2 - 1 + i;
+	// 		int y = 256 / 2 - 1 + j;
+
+	// 		real_fuv2[i][j] = sobel_mask[i][j];
+	// 		image_fuv2[i][j] = 0;
+	// 	}
+	// }
+
+	// fft2d(256, 256, real_fuv2, image_fuv2, 1);
 }
 
 void experiment4(char fname[])
 {
 	ImageType baseImage(512, 512, 255);
+	ImageType paddedImage(1024, 1024, 255);
 	readImage(fname, baseImage);
 
-	// Step 1 - Ln f(x,y)
+	// Padding base image
 	int temp;
 	for (int i = 0; i < 512; i++)
 	{
 		for (int j = 0; j < 512; j++)
 		{
+
 			baseImage.getPixelVal(i, j, temp);
+			paddedImage.setPixelVal(i, j, temp);
+		}
+	}
+	for (int i = 512; i < 1024; i++)
+	{
+		for (int j = 512; j < 1024; j++)
+		{
+			paddedImage.setPixelVal(i, j, 0);
+		}
+	}
+
+	// Step 1 Ln f(x,y)
+	for (int i = 0; i < 1024; i++)
+	{
+		for (int j = 0; j < 1024; j++)
+		{
+			paddedImage.getPixelVal(i, j, temp);
 			temp = log(temp);
-			baseImage.setPixelVal(i, j, temp);
+			paddedImage.setPixelVal(i, j, temp);
 		}
 	}
 
 	// Step 2 FT
-	double **real_fuv = new double *[512];
-	for (int i = 0; i < 512; i++)
+	double **real_fuv = new double *[1024];
+	for (int i = 0; i < 1024; i++)
 	{
-		real_fuv[i] = new double[512 * 2 + 1];
+		real_fuv[i] = new double[1024 * 2 + 1];
 	}
-	double **image_fuv = new double *[512];
-	for (int i = 0; i < 512; i++)
+	double **image_fuv = new double *[1024];
+	for (int i = 0; i < 1024; i++)
 	{
-		image_fuv[i] = new double[512 * 2 + 1];
+		image_fuv[i] = new double[1024 * 2 + 1];
 	}
 
-	for (int i = 0; i < 512; i++)
+	for (int i = 0; i < 1024; i++)
 	{
-		for (int j = 0; j < 512; j++)
+		for (int j = 0; j < 1024; j++)
 		{
-			baseImage.getPixelVal(i, j, temp);
+			paddedImage.getPixelVal(i, j, temp);
 			real_fuv[i][j] = temp;
 			image_fuv[i][j] = 0;
 		}
 	}
-	fft2d(512, 512, real_fuv, image_fuv, -1);
+	fft2d(1024, 1024, real_fuv, image_fuv, 1);
+
 	// Shift the spectrum
-	for (int i = 0; i < 512; i++)
+	for (int i = 0; i < 1024; i++)
 	{
-		for (int j = 0; j < 512; j++)
+		for (int j = 0; j < 1024; j++)
 		{
 			real_fuv[i][j] = real_fuv[i][j] * pow(-1, i + j);
 			image_fuv[i][j] = image_fuv[i][j] * pow(-1, i + j);
@@ -92,18 +234,43 @@ void experiment4(char fname[])
 	float gamma_H = 1.5, gamma_L = .5, c = 1, D_0 = 1.8;
 	float coef = -c / D_0 / D_0;
 	float gammaDiff = gamma_H - gamma_L;
-	for (int i = 0; i < 512; i++)
+	for (int i = 0; i < 1024; i++)
 	{
-		for (int j = 0; j < 512; j++)
+		for (int j = 0; j < 1024; j++)
 		{
-			int i_adj = i - 512 / 2, j_adj = j - 512 / 2;
+			int i_adj = i - 1024 / 2, j_adj = j - 1024 / 2;
 			real_fuv[i][j] *= gammaDiff * (1 - exp(coef * (i_adj * i_adj + j_adj * j_adj))) + gamma_L;
 			image_fuv[i][j] *= gammaDiff * (1 - exp(coef * (i_adj * i_adj + j_adj * j_adj))) + gamma_L;
 		}
 	}
 
 	// Step 4 Inverse FT
-	fft2d(512, 512, real_fuv, image_fuv, -1);
+	fft2d(1024, 1024, real_fuv, image_fuv, -1);
+
+	// Normalization
+	float rmax = real_fuv[0][0];
+	float rmin = real_fuv[0][0];
+	for (int i = 0; i < 1024; i++)
+	{
+		for (int j = 0; j < 1024; j++)
+		{
+			if (real_fuv[i][j] > rmax)
+			{
+				rmax = real_fuv[i][j];
+			}
+			if (real_fuv[i][j] < rmin)
+			{
+				rmin = real_fuv[i][j];
+			}
+		}
+	}
+	for (int i = 0; i < 1024; i++)
+	{
+		for (int j = 0; j < 1024; j++)
+		{
+			real_fuv[i][j] = 255 * (real_fuv[i][j] - rmin) / (rmax - rmin);
+		}
+	}
 
 	// Step 5 Take exp
 	ImageType finalImage(512, 512, 255);
@@ -167,274 +334,6 @@ void test2dfft()
 	}
 	delete[] image_fuv;
 }
-
-/*
-void part2(char fname[], int size)
-{
-	ImageType baseImage(512, 512, 255);
-	readImage(fname, baseImage);
-
-	double **real_fuv = new double *[512];
-	for (int i = 0; i < 512; i++)
-	{
-		real_fuv[i] = new double[512 * 2 + 1];
-	}
-	double **image_fuv = new double *[512];
-	for (int i = 0; i < 512; i++)
-	{
-		image_fuv[i] = new double[512 * 2 + 1];
-	}
-
-	int temp;
-	for (int i = 0; i < 512; i++)
-	{
-		for (int j = 0; j < 512; j++)
-		{
-			baseImage.getPixelVal(i, j, temp);
-			real_fuv[i][j] = temp;
-			image_fuv[i][j] = 0;
-		}
-	}
-
-	// FFT2D for no shift
-	fft2d(512, 512, real_fuv, image_fuv, -1);
-	// Spectrum no shift
-	double spectrum[512][512];
-	for (int i = 0; i < 512; i++)
-	{
-		for (int j = 0; j < 512; j++)
-		{
-			spectrum[i][j] = sqrt(real_fuv[i][j] * real_fuv[i][j] + image_fuv[i][j] + image_fuv[i][j]);
-		}
-	}
-	// Inverse for shift
-	fft2d(512, 512, real_fuv, image_fuv, 1);
-
-	// Shift the spectrum
-	for (int i = 0; i < 512; i++)
-	{
-		for (int j = 0; j < 512; j++)
-		{
-			real_fuv[i][j] = real_fuv[i][j] * pow(-1, i + j);
-			image_fuv[i][j] = image_fuv[i][j] * pow(-1, i + j);
-		}
-	}
-
-	// Call 2DFFT
-	fft2d(512, 512, real_fuv, image_fuv, -1);
-
-	double magnitude[512][512];
-	// Calculate magnitude
-	for (int i = 0; i < 512; i++)
-	{
-		for (int j = 0; j < 512; j++)
-		{
-			magnitude[i][j] = sqrt(real_fuv[i][j] * real_fuv[i][j] + image_fuv[i][j] + image_fuv[i][j]);
-		}
-	}
-
-	// Apply Log for better visualization
-	for (int i = 0; i < 512; i++)
-	{
-		for (int j = 0; j < 512; j++)
-		{
-			magnitude[i][j] = log(1 + magnitude[i][j]);
-			spectrum[i][j] = log(1 + spectrum[i][j]);
-		}
-	}
-
-	if (size == 32)
-	{
-		char tempname[] = "fft_32.pgm";
-		char tempname2[] = "fft_unshifted_32.pgm";
-		ImageType final(512, 512, 255);
-		ImageType final2(512, 512, 255);
-
-		// Normalization
-		float rmax = magnitude[0][0];
-		float rmin = magnitude[0][0];
-		float rmax2 = spectrum[0][0];
-		float rmin2 = spectrum[0][0];
-		for (int i = 0; i < 512; i++)
-		{
-			for (int j = 0; j < 512; j++)
-			{
-				if (magnitude[i][j] > rmax)
-				{
-					rmax = magnitude[i][j];
-				}
-				if (magnitude[i][j] < rmin)
-				{
-					rmin = magnitude[i][j];
-				}
-				if (spectrum[i][j] > rmax2)
-				{
-					rmax2 = spectrum[i][j];
-				}
-				if (spectrum[i][j] < rmin2)
-				{
-					rmin2 = spectrum[i][j];
-				}
-			}
-		}
-		for (int i = 0; i < 512; i++)
-		{
-			for (int j = 0; j < 512; j++)
-			{
-				magnitude[i][j] = 255 * (magnitude[i][j] - rmin) / (rmax - rmin);
-				spectrum[i][j] = 255 * (spectrum[i][j] - rmin2) / (rmax2 - rmin2);
-			}
-		}
-
-		int temp2;
-		int temp3;
-		// Write to image
-		for (int i = 0; i < 512; i++)
-		{
-			for (int j = 0; j < 512; j++)
-			{
-				temp3 = spectrum[i][j];
-				temp2 = magnitude[i][j];
-				final.setPixelVal(i, j, temp2);
-				final2.setPixelVal(i, j, temp3);
-			}
-		}
-
-		writeImage(tempname, final);
-		writeImage(tempname2, final2);
-	}
-	else if (size == 64)
-	{
-		char tempname[] = "fft_64.pgm";
-		char tempname2[] = "fft_unshifted_64.pgm";
-		ImageType final(512, 512, 255);
-		ImageType final2(512, 512, 255);
-
-		// Normalization
-		float rmax = magnitude[0][0];
-		float rmin = magnitude[0][0];
-		float rmax2 = spectrum[0][0];
-		float rmin2 = spectrum[0][0];
-		for (int i = 0; i < 512; i++)
-		{
-			for (int j = 0; j < 512; j++)
-			{
-				if (magnitude[i][j] > rmax)
-				{
-					rmax = magnitude[i][j];
-				}
-				if (magnitude[i][j] < rmin)
-				{
-					rmin = magnitude[i][j];
-				}
-				if (spectrum[i][j] > rmax2)
-				{
-					rmax2 = spectrum[i][j];
-				}
-				if (spectrum[i][j] < rmin2)
-				{
-					rmin2 = spectrum[i][j];
-				}
-			}
-		}
-		for (int i = 0; i < 512; i++)
-		{
-			for (int j = 0; j < 512; j++)
-			{
-				magnitude[i][j] = 255 * (magnitude[i][j] - rmin) / (rmax - rmin);
-				spectrum[i][j] = 255 * (spectrum[i][j] - rmin2) / (rmax2 - rmin2);
-			}
-		}
-
-		int temp2;
-		int temp3;
-		// Write to image
-		for (int i = 0; i < 512; i++)
-		{
-			for (int j = 0; j < 512; j++)
-			{
-				temp3 = spectrum[i][j];
-				temp2 = magnitude[i][j];
-				final.setPixelVal(i, j, temp2);
-				final2.setPixelVal(i, j, temp3);
-			}
-		}
-
-		writeImage(tempname, final);
-		writeImage(tempname2, final2);
-	}
-	if (size == 128)
-	{
-		char tempname[] = "fft_128.pgm";
-		char tempname2[] = "fft_unshifted_128.pgm";
-		ImageType final(512, 512, 255);
-		ImageType final2(512, 512, 255);
-
-		// Normalization
-		float rmax = magnitude[0][0];
-		float rmin = magnitude[0][0];
-		float rmax2 = spectrum[0][0];
-		float rmin2 = spectrum[0][0];
-		for (int i = 0; i < 512; i++)
-		{
-			for (int j = 0; j < 512; j++)
-			{
-				if (magnitude[i][j] > rmax)
-				{
-					rmax = magnitude[i][j];
-				}
-				if (magnitude[i][j] < rmin)
-				{
-					rmin = magnitude[i][j];
-				}
-				if (spectrum[i][j] > rmax2)
-				{
-					rmax2 = spectrum[i][j];
-				}
-				if (spectrum[i][j] < rmin2)
-				{
-					rmin2 = spectrum[i][j];
-				}
-			}
-		}
-		for (int i = 0; i < 512; i++)
-		{
-			for (int j = 0; j < 512; j++)
-			{
-				magnitude[i][j] = 255 * (magnitude[i][j] - rmin) / (rmax - rmin);
-				spectrum[i][j] = 255 * (spectrum[i][j] - rmin2) / (rmax2 - rmin2);
-			}
-		}
-
-		int temp2;
-		int temp3;
-		// Write to image
-		for (int i = 0; i < 512; i++)
-		{
-			for (int j = 0; j < 512; j++)
-			{
-				temp3 = spectrum[i][j];
-				temp2 = magnitude[i][j];
-				final.setPixelVal(i, j, temp2);
-				final2.setPixelVal(i, j, temp3);
-			}
-		}
-
-		writeImage(tempname, final);
-		writeImage(tempname2, final2);
-	}
-
-	for (int i = 0; i < 512; ++i)
-	{
-		delete[] real_fuv[i];
-	}
-	delete[] real_fuv;
-	for (int i = 0; i < 512; ++i)
-	{
-		delete[] image_fuv[i];
-	}
-	delete[] image_fuv;
-}*/
 
 void imgtodata(ImageType &image, float **data)
 {
